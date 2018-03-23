19465b246e71e18088b8d0bf038332e0547f8474
Jon <jondekerh@gmail.com>
2018-03-23 16:37:38 -0700

    fixed typos and stuff

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -10,7 +10,7 @@ class Homepage extends Component {
         </h1>
 
         <p>
-          Hi, I'm Jon Houston. I'm a Javascript and Python programmer with an interest in p2p technology and machine learning. I'm self taught and always trying to learn something new. When I'm not coding I like to write, stay active, and read history books. If you want to know more, try asking BOT_NAME some questions about me!
+          Hi, I'm Jon Houston. I'm a Javascript and Python programmer with an interest in p2p technology and machine learning. I'm self taught and always learning something new. When I'm not coding I like to write, stay active, and read about history. If you want to know more, try asking BOT_NAME some questions about me!
         </p>
 
       </div>
